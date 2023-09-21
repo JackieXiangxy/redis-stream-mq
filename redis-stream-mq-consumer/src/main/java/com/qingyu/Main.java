7261270bf924c4e8fe3b274ec73e914bc6feb913
jackie <jackiexiang@foxmail.com>
2023-09-21 18:19:48 +0800

    init

---
+++ redis-stream-mq-consumer/src/main/java/com/qingyu/Main.java
@@ -0,0 +1,11 @@
+package com.qingyu;
+
+/**
+ * @author jackie
+ * @since 2.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
