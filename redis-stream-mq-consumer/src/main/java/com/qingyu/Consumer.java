df75fd5327b0e8c37b7c90c6f260788f903a71d9
jackie <xiangyujkrg@gmail.com>
2023-09-21 18:45:39 +0800

    提交代码

---
+++ redis-stream-mq-consumer/src/main/java/com/qingyu/Consumer.java
@@ -0,0 +1,15 @@
+package com.qingyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jackie
+ * @since 2.0
+ */
+@SpringBootApplication
+public class Consumer {
+    public static void main(String[] args) {
+        SpringApplication.run(Consumer.class,args);
+    }
+}
---
+++ redis-stream-mq-producer/src/main/java/com/qingyu/Producer.java
@@ -0,0 +1,15 @@
+package com.qingyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jackie
+ * @since 2.0
+ */
+@SpringBootApplication
+public class Producer {
+    public static void main(String[] args) {
+        SpringApplication.run(Producer.class,args);
+    }
+}
